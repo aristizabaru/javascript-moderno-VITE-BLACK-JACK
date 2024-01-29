e79e3cea5a1147b040772388aa3cf756c67e8b4f
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-01-28 21:39:45 -0500

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# javascript-moderno-VITE-BLACK-JACK
-Este es un repositorio personal para ejecución de los proyectos del curso JavaScript Moderno: Guía para dominar el lenguaje de Fernando Herrera en Udemy.
+# JavaScript Moderno: Guía para dominar el lenguaje (VITE - Black Jack)
+
+## Acerca de
+
+Este es un repositorio personal para ejecución del proyecto del curso **JavaScript Moderno: Guía para dominar el lenguaje** de **Fernando Herrera** en la plataforma Udemy. Para acceder al curso completo puede hacer [clic aquí](https://www.udemy.com/course/javascript-fernando-herrera/)
+
+El proyecto desarrollado a continuación es un juego de Black Jack para explorar conceptos fundamentales del lenguaje. Dentro de la configuración y desarrollo del proyecto el tema principal es el uso de [Vite](https://vitejs.dev/) como bundler.
+
+## Requerimientos
+
+- Node 20.9.0 LTS
+
+## Instalación del proyecto
+
+Para instalar el proyecto siga los siguientes pasos
+
+Instalar módulos o dependencias
+
+```
+npm install
+```
+
+## Ejecución del proyecto
+
+Para ejecutar el proyecto se deben seguir los siguientes pasos:
+
+1. Corre el proyecto usando alguno de los siguientes scripts según el entorno
+
+Ejecutar entorno de desarrollo
+
+```
+npm run dev
+```
+
+Generar build de producción
+
+```
+npm run build
+```
+
+
---
+++ main.js
@@ -0,0 +1,153 @@
+import './style.css'
+
+let deck = []
+
+const tipos = ['C', 'D', 'H', 'S'],
+  especiales = ['A', 'J', 'Q', 'K']
+
+let puntosJugadores = []
+
+// Referencias HTLM
+const btnPedir = document.querySelector('#btnPedir'),
+  btnDetener = document.querySelector('#btnDetener'),
+  btnNuevo = document.querySelector('#btnNuevo')
+
+const puntosHTLM = document.querySelectorAll('small'),
+  divCartaJugadores = document.querySelectorAll('.divCartas')
+
+const inicializarJuego = (numJugadores = 2) => {
+  crearDeck()
+  deck = shuffleDeck(deck)
+
+  puntosJugadores = []
+  for (let index = 0; index < numJugadores; index++) {
+    puntosJugadores.push(0)
+    puntosHTLM[index].innerText = 0
+    divCartaJugadores[index].innerHTML = ''
+  }
+
+  activarBotones()
+}
+
+const crearDeck = () => {
+  deck = []
+
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(`${i}${tipo}`)
+    }
+  }
+
+  for (let tipo of tipos) {
+    for (let esp of especiales) {
+      deck.push(`${esp}${tipo}`)
+    }
+  }
+}
+
+const shuffleDeck = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck'
+  }
+  return deck.pop()
+}
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1)
+
+  return (isNaN(valor)) ?
+    (valor === 'A') ? 11 : 10
+    : valor * 1
+}
+
+// Último indice del array es el turno de la computadora
+const acumularPuntos = (carta, turno) => {
+  puntosJugadores[turno] += valorCarta(carta)
+  puntosHTLM[turno].innerText = puntosJugadores[turno]
+
+  return puntosJugadores[turno]
+}
+
+const crearCarta = (carta, turno) => {
+  const imgCarta = document.createElement('img')
+  imgCarta.classList.add('carta')
+  imgCarta.src = `./assets/cartas/${carta}.png`
+  divCartaJugadores[turno].append(imgCarta)
+}
+
+const turnoComputadora = (puntosMinimos) => {
+
+  let puntosComputadora = 0
+
+  do {
+    const carta = pedirCarta()
+    puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1)
+    crearCarta(carta, puntosJugadores.length - 1)
+
+    if (puntosMinimos > 21) break
+
+  } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21))
+
+  setTimeout(() => {
+    determinarGanador()
+  }, 100)
+}
+
+function determinarGanador() {
+
+  const [puntosJugador, puntosComputadora] = puntosJugadores
+
+  if (puntosComputadora === puntosJugador) {
+    alert('Nadie gana')
+  } else if (puntosJugador > 21) {
+    alert('Computadora gana')
+  } else if (puntosComputadora > 21) {
+    alert('Jugador gana')
+  } else {
+    alert('Computadora gana')
+  }
+}
+
+function desactivarBotones() {
+  btnPedir.disabled = true
+  btnDetener.disabled = true
+}
+
+function activarBotones() {
+  btnPedir.disabled = false
+  btnDetener.disabled = false
+}
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+  const carta = pedirCarta()
+  const puntosJugador = acumularPuntos(carta, 0)
+  crearCarta(carta, 0)
+
+  if (puntosJugador >= 21) {
+    desactivarBotones()
+    turnoComputadora(puntosJugadores[0])
+  }
+
+})
+
+btnDetener.addEventListener('click', () => {
+  desactivarBotones()
+  turnoComputadora(puntosJugadores[0])
+})
+
+btnNuevo.addEventListener('click', () => {
+  inicializarJuego()
+})
+
+
+
+
